8703b1f7f751e40d96c97e796d13dc79280e1798
LSA9 <LSA9@pitt.edu>
2015-10-13 14:11:00 -0400

    Update OutsideCityChecker.java

--- src/OutsideCityChecker.java
+++ src/OutsideCityChecker.java
@@ -4,10 +4,10 @@ public class OutsideCityChecker {
 	public OutsideCityChecker(){
 	}
 	
-	public String checker(StringMaker stringMaker, int carNumber){
+	public String checker(StringMaker stringMaker, int carNumber, int firstIteration){
 		String s = stringMaker.getString();
 		String[] tokens = s.split(" ");
-		if((tokens[6].equals("Outside") && tokens[7].equals("City")) || (tokens[7].equals("Outside") && tokens[8].equals("City"))){
+		if((tokens[6].equals("Outside") && tokens[7].equals("City")) || (tokens[7].equals("Outside") && tokens[8].equals("City")) && firstIteration!=1){
 			String newString = "Driver "+ carNumber +" has left the city!";
 			stringMaker.setString(newString);
 			s = newString;
@@ -17,3 +17,4 @@ public class OutsideCityChecker {
 
 
 }
+
